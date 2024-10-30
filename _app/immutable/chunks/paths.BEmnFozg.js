0456a09c712f712a2df897eb2e5eea9f4ab8fe4e
juanfuc <juanfuc@gmail.com>
2024-10-30 11:15:45 -0500

    Updates

---
+++ _app/immutable/chunks/paths.BEmnFozg.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_kh464d)==null?void 0:s.base)??"/montajes_aep";var a;const t=((a=globalThis.__sveltekit_kh464d)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.Bk6Fbi_a.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BWQAC9w1.js";export{t as start};
